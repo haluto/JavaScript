9dd4539998b03f9b10d4465a8bb233b54428971e
terry.yin <haluto8086@sina.cn>
2018-04-13 14:09:00 +0800

    react: lists and keys

--- react/my-app/src/index.js
+++ react/my-app/src/index.js
@@ -11,6 +11,7 @@ import {l003_1, l003_2, l003_3} from './js/l003_components_and_props';
 import {l004_1, l004_2, l004_3, l004_4, l004_5, l004_6} from './js/l004_state_and_lifecycle';
 import {l005_1, l005_2} from './js/l005_handling_events';
 import {l006_1, l006_2, l006_3, l006_4} from './js/l006_conditional_rendering';
+import {l007_1, l007_2, l007_3, l007_4, l007_5} from './js/l007_lists_and_keys';
 
 import {l011_1, l011_2} from './js/l011_thinking_in_react';
 //terry add for lesson. end.
@@ -45,7 +46,12 @@ registerServiceWorker();
 //l006_1();
 //l006_2();
 //l006_3();
-l006_4();
+//l006_4();
+//l007_1();
+//l007_2();
+//l007_3();
+//l007_4();
+l007_5();
 
 //l011_1();
 //l011_2();
